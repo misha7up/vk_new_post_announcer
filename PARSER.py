d0a0c478d74ce7c9603e9af96251eb93f3b4de71
misha7up <mike.prokofev@gmail.com>
2023-07-19 01:25:07 +0300

    Major fixes: add requirements, README and .gitignore files. Code now supports flake8 and includes type hints & docstrings.

--- PARSER.py
+++ PARSER.py
@@ -1,40 +1,55 @@
-import vk_api, json, time
+import vk_api
+import time
 
-LOGIN = ''
-SERVICE_TOKEN = ''
-API_VERSION = '5.131'
-POST_COUNT = '1'
-DELAY = 120
-DOMAIN_BASE = [{'id': 'renaultru', 'name': 'RENAULT RUSSIA'}, {'id': 'nissanrussia', 'name': 'NISSAN RUSSIA'}, {'id':'mitsucarrus', 'name': 'MITSUBISHI RUSSIA'}]
-PostDataBase = []
+LOGIN: str = ''  # Формат XXXXXXXXXXX. Номер для входа
+SERVICE_TOKEN: str = ''
+API_VERSION: str = '5.131'  # Версия API Вконтакте. Дефолт - 5.131
+POST_COUNT: str = '1'  # Количество постов для парсинга. Дефолт - парсим 1 пост
+DELAY: int = 120  # Задержка между поисками новых постов
+DOMAIN_BASE: list[dict] = [{'id': 'renaultru', 'name': 'RENAULT RUSSIA'},
+                           {'id': 'nissanrussia', 'name': 'NISSAN RUSSIA'},
+                           {'id': 'mitsucarrus', 'name': 'MITSUBISHI RUSSIA'}]
+PostDataBase: list[str] = []
+
 
 def vk_auth():
+    """Авторизоваться во ВКонтакте через API."""
     global vk
     vk_session = vk_api.VkApi(login=LOGIN, token=SERVICE_TOKEN)
     vk_session.auth(reauth=False, token_only=True)
     vk = vk_session.get_api()
 
+
 def get_info(domain_name):
-    post = vk.wall.get(domain=domain_name, count=POST_COUNT, v=API_VERSION, access_token=SERVICE_TOKEN)
-    return(post)
+    """Получить последний пост со стены группы."""
+    post: str = vk.wall.get(domain=domain_name, count=POST_COUNT,
+                            v=API_VERSION, access_token=SERVICE_TOKEN)
+    return post
+
 
 def parse_post(post_info):
-    post_id = post_info["items"][0]["id"]
-    post_date = post_info["items"][0]["date"]
-    post_text = post_info["items"][0]["text"]
-    post_data = {'id': post_id, 'date': post_date, 'text': post_text}
-    post_uniq_id = str(post_id) + str(post_date)
-    return(post_uniq_id, post_data)
+    """Распарсить пост и его содержимое."""
+    post_id: str = post_info["items"][0]["id"]
+    post_date: str = post_info["items"][0]["date"]
+    post_text: str = post_info["items"][0]["text"]
+    post_data: dict[str: str] = {'id': post_id, 'date': post_date,
+                                 'text': post_text}
+    post_uniq_id: str = str(post_id) + str(post_date)
+    return post_uniq_id, post_data
+
 
 def check_post(post_info, group_name):
-    post_uniq_id = post_info[0]
-    post_data = post_info[1]
-    if post_uniq_id not in PostDataBase: 
+    """Проверка поста на уникальность и его наличие в БД."""
+    post_uniq_id: str = post_info[0]
+    post_data: str = post_info[1]
+    if post_uniq_id not in PostDataBase:
         PostDataBase.append(post_uniq_id)
-        post_text = post_data["text"]
-        return('Post with ID: ' + post_uniq_id + ' has been added from: ' + group_name)
+        post_text: str = post_data["text"]
+        return (f'Запись с ID «{post_uniq_id}» '
+                'была добавлена из группы «{group_name}»')
     else:
-        return('No new posts in ' + group_name + '!')
+        return f'В группе «{group_name}» не найдено новых записей!'
+
 
 vk_auth()
 
@@ -44,5 +59,5 @@ while True:
         post_info = get_info(DOMAIN_BASE[domain]["id"])
         parsed_post = parse_post(post_info)
         print(check_post(parsed_post, group_name))
-        
+
     time.sleep(DELAY)
---
+++ README.md
@@ -0,0 +1,33 @@
+## Скрипт для получения уведомлений о выходе новых записей ВКонтакте в отслеживаемых группах в личные сообщения.
+
+
+## Инструкции по установке
+***- Клонируйте репозиторий:***
+```
+git clone git@github.com:misha7up/vk_new_post_announcer.git
+```
+
+***- Установите и активируйте виртуальное окружение:***
+- для MacOS
+```
+python3 -m venv venv
+```
+- для Windows
+```
+python -m venv venv
+source venv/bin/activate
+source venv/Scripts/activate
+```
+
+***- Установите зависимости из файла requirements.txt:***
+```
+pip install -r requirements.txt
+
+***- Как запустить парсер:***
+- для MacOS
+```
+python3 parser.py
+```
+- для Windows
+```
+python parser.py
